7b0cf6bab16bdaf2aba707137286443dbfcb1d4a
naiyar.2000 <naiyarimam.2000@gmail.com>
2020-10-08 23:17:44 +0530

    added likes api for post

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
+const { remove } = require('../../models/Post');
 
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
@@ -113,7 +114,34 @@ router.put('/like/:id', auth, async (req, res) => {
             return res.status(400).json({msg: 'Post already liked'});
         }
 
-        post.likes.ushift({user: req.user.id});
+        post.likes.unshift({user: req.user.id});
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+
+//@route    PUT api/post/unlike/:id
+//@desc     Unlike a post 
+//@access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Check if post already liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({msg: 'Post has not yet been liked'});
+        }
+
+        //GEt remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
 
         await post.save();
 
