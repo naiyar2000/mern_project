445f2f67a63b544462029901d62def5e3731615a
naiyar.2000 <naiyarimam.2000@gmail.com>
2020-10-18 02:22:25 +0530

    action and reducer for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
@@ -7,27 +7,41 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 
+
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './action/auth';
 
 
-const App = () => ( 
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+      return( 
+      <Provider store={store}>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <section className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/register" component={Register}/>
+                <Route exact path="/login" component={Login}/>
+              </Switch>
+            </section>
+          </Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 
 export default App;
--- client/src/action/alert.js
+++ client/src/action/alert.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid/v4';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout=5000) => dispatch => {
     const id = uuid();
     dispatch({
         type: SET_ALERT,
@@ -11,5 +11,5 @@ export const setAlert = (msg, alertType) => dispatch => {
     setTimeout(() => dispatch({
         type: REMOVE_ALERT,
         payload: id
-    }), 5000)
+    }), timeout)
 }
